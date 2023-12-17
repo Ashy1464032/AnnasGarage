f027ebe93efdd689590ca24d1303d62b4f6b6416
Ashy146 <Ashy1464032@gmail.com>
2023-12-17 13:09:24 +0000

    Made the Bill Display text box readOnly

--- Bill.Designer.cs
+++ Bill.Designer.cs
@@ -46,6 +46,7 @@ namespace AnnasGarage
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(149, 88);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(234, 44);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Your Bill Total";
