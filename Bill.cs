e6c70f16ecf4372fc98dab92745c0ec53256964b
Ashton Skipworth <Ashy1464032@gmail.com>
2023-12-19 09:52:00 +0000

    bug fixes in Bill.cs

--- Bill.cs
+++ Bill.cs
@@ -28,6 +28,7 @@ namespace AnnasGarage
 
         private void Bill_FormClosed(object sender, FormClosedEventArgs e)
         {
+            
             Form1 form1 = new Form1();
             form1.Show();
             Form1.TotalBill = 0.00;
